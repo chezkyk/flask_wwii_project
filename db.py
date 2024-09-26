85255c1550baf1e9da75c01e2392750c23612530
chezkyk <Y0548532123@GMAIL.COM>
2024-09-26 18:00:28 +0300

    added db

---
+++ db.py
@@ -0,0 +1,25 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+import psycopg2
+from psycopg2 import pool
+
+
+connection_pool = psycopg2.pool.SimpleConnectionPool(
+    minconn=1,
+    maxconn=10,
+    dbname="wwii_missions",
+    user="postgres",
+    password="1234",
+    host="localhost",
+    port="5432"
+)
+
+def get_db_connection():
+    if connection_pool:
+        conn = connection_pool.getconn()
+        return conn
+
+def release_db_connection(conn):
+    connection_pool.putconn(conn)
